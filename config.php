6127bf957bd91963bca8a212d46ae18cec2701c7
Jasonmpal <jasonmpal@knights.ucf.edu>
2018-08-25 14:25:46 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // from: http://php.thedemosite.co.uk/
 // version 1.2
-define('DBHOST', 'localhost');// MySQL host address - localhost is usually fine
-define('DBUSER', ''); // db username
-define('DBPASS', ''); // db password
-define('DBNAME', ''); // db name
+define('DBHOST', 'aa16glunvbqnu58.ctogpmoawefq.us-east-1.rds.amazonaws.com');// MySQL host address - localhost is usually fine
+define('DBUSER', 'JPalatucci'); // db username
+define('DBPASS', 'PolySly87'); // db password
+define('DBNAME', 'testDB'); // db name
 
 define('DEBUG', false);// set to false when done testing
 
